d8461ad3f84f5c9af2012c56b2658fb6528cd086
ROBERT GRAHAM CLARK <robertgrahamclark@gmail.com>
2015-12-16 10:30:06 +1100

    First commit

---
+++ R/data.R
@@ -0,0 +1,4 @@
+#' An example sample data file of 5000 respondents.
+#'
+#' A dataset containing the values of y (a variable of interest which is outlier-prone), and two auxiliary variables x1 and x2.
+"survdat.example"
---
+++ R/robust.lm.onesided.R
@@ -0,0 +1,38 @@
+#' robust.lm.onesided
+#'
+#' This function performs robust regression using M-estimation
+#' using the one-sided Huber function, with residuals truncated at Q / (data$gregwt-1)
+#' where data$gregwt is the generalized regression weight
+#' @param formula The regression formula (e.g. income ~ employment + old.turnover if income is survey variable and employment and old.turnover are auxiliary variables).
+#' @param data A data frame including the variables in formula, and gregwt (generalized regression estimator weight).
+#' @param Q The tuning parameter where large Q corresponds to no outlier treatment, and small Q corresponds to many outliers being flagged.
+#' @param maxit The maximum number of iterations.
+#' @param stop Set to T to open a browser window (for debugging purposes)
+#' @details
+#' Uses iteratively reweighted least squares.
+#' @return The final linear model fit (an object of class "lm").
+#' @references
+#' Clark, R. G. (1995), "Winsorisation methods in sample surveys," Masters thesis, Australian National University, http://hdl.handle.net/10440/1031.
+#'
+#' Kokic, P. and Bell, P. (1994), "Optimal winsorizing cutoffs for a stratified finite population estimator," J. Off. Stat., 10, 419–435.
+#' @examples
+#' robust.lm.onesided(formula=y~x1+x2,data=survdat.example,Q=250)
+
+robust.lm.onesided <- function( formula , data , Q , maxit=100,stop=F){
+  if(stop) browser()
+  y <- data[,as.character(formula)[2]]
+  robust.resid <- (y-median(y))/(1.483*(quantile(y,probs=0.75)-quantile(y,probs=0.25)))
+  data$irls.w <- pmin(robust.resid,2) / robust.resid
+  old.beta <- rep(0,(length(as.character(formula))-2))
+  delta <- 1
+  iter <- 1
+  while((iter<=100)&(delta>=1e-6)){
+    wfit <- lm(formula=formula,data=data,weights=irls.w)
+    #cutoffs <- wfit$fitted.values + Q / (data$gregwt-1)
+    data$irls.w <- pmin( y - wfit$fitted.values , Q / (data$gregwt-1) ) / ( y - wfit$fitted.values )
+    delta <- sqrt(sum((wfit$coef-old.beta)^2))
+    old.beta <- wfit$coef
+    iter <- iter+1
+  }
+  wfit
+}
